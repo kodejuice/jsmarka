72354b36ee0b4b9306efe190720d235732b126b9
KodeJuice <kodejuice@gmail.com>
2017-10-13 15:37:44 +0100

    fixed paging bug

--- server/lib/paging.js
+++ server/lib/paging.js
@@ -1,38 +1,40 @@
 /*
-    Paging script for jsmarka
+	Paging script for jsmarka
 
-    Sochima Biereagu, @KodeJuice
+	Sochima Biereagu, @KodeJuice
 */
 
 const [min, max, ceil] = [Math.min, Math.max, Math.ceil];
 
 
 function _link(url, name, start, isCurrent = false) {
-    return '<li>'+(isCurrent ?
-        ` <span class='paging-link current'>${name}</span> ` :
-        ` <a class='paging-link' href="${url}&s=${start}"> ${name} </a>`) + '</li>';
+	return '<li>'+(isCurrent ?
+		` <span class='paging-link current'>${name}</span> ` :
+		` <a class='paging-link' href="${url}&s=${start}"> ${name} </a>`) + '</li>';
 }
 
 function paging(url, all, startAt, maxItems) {
-    maxItems -= 1;
+	maxItems -= 1;
 
-    let pages = ceil(all / maxItems);
-    let curpage = (startAt / maxItems);
+	let pages = ceil(all / maxItems);
+	let curpage = ~~(startAt / maxItems);
 
-    let res = `<nav center aria-label="Page navigation">
-                  <ul class="pagination">`;
+	let res = `<nav center aria-label="Page navigation">
+				  <ul class="pagination">`;
 
-    if (all > maxItems){
-        res += (startAt > 0) ? _link(url, "Prev", ~~(curpage - 1)) : "";
+	if (all > maxItems){
+		res += (startAt > 0) ? _link(url, "Prev", curpage - 1) : "";
 
-        for (let x = max(1, curpage - 5); x < min(curpage + 5, pages); x += 1)
-            res += _link(url, ~~x, ~~x, ~~x === ~~curpage);
-    
-        res += ((curpage + 1 <= pages) ? _link(url, "Next", ~~(curpage + 1)) : "");
-    }
+		for (let x = max(1, curpage - 5); x < min(curpage + 5, pages); x += 1){
+			x = ~~x;
+			res += _link(url, x, x, x === curpage);
+		}
+	
+		res += ((curpage + 1 <= pages) ? _link(url, "Next", curpage + 1) : "");
+	}
 
 
-    return `${res} </ul> </nav>`;
+	return `${res} </ul> </nav>`;
 }
 
 
